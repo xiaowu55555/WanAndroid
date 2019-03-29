e199d5626797cd764769ff4e520026ef7bed1ee4
libing <512359638>
2019-03-29 17:02:29 +0800

    关于我们

--- app/src/main/java/com/bingo/wanandroid/ui/me/AboutActivity.java
+++ app/src/main/java/com/bingo/wanandroid/ui/me/AboutActivity.java
@@ -2,33 +2,42 @@ package com.bingo.wanandroid.ui.me;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.bingo.wanandroid.BuildConfig;
 import com.bingo.wanandroid.R;
+import com.frame.library.base.BaseActivity;
+import com.frame.library.base.BaseSwipeBackActivity;
 import com.frame.library.base.BaseViewModel;
 import com.frame.library.base.WebViewActivity;
 import com.frame.library.widget.TitleBar;
 
-public class AboutActivity extends WebViewActivity {
+public class AboutActivity extends BaseSwipeBackActivity {
+
+    private TextView tv_content;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, AboutActivity.class);
         context.startActivity(starter);
     }
 
-    @Override
-    protected ViewGroup getWebContentView() {
-        return (ViewGroup) findViewById(R.id.fl_content);
-    }
-
-    @Override
-    protected void getIntentData() {
-        url = "https://www.wanandroid.com/about";
-    }
 
     @Override
     protected int getLayoutRes() {
         return R.layout.activity_about;
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+        tv_content = findViewById(R.id.tv_content);
+        tv_content.setMovementMethod(LinkMovementMethod.getInstance());
+        tv_content.setText(Html.fromHtml(getString(R.string.about_content)));
+        TextView tv_version = findViewById(R.id.tv_version);
+        tv_version.setText("V " + BuildConfig.VERSION_NAME);
     }
 
     @Override
